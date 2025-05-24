01f41c0263fb84932cff08f0d8281810109aba18
Your Name <you@example.com>
2025-05-25 00:46:43 +0400

    Актуализированна аутентификация и добавлен httr2

--- R/rym_auth.R
+++ R/rym_auth.R
@@ -2,7 +2,7 @@ rym_auth <-
   function(login = getOption("rym.user"), 
            new.user = FALSE, 
            token.path = getOption("rym.token_path")) {
-
+    
     # check path
     if ( is.null(token.path) ) {
       
@@ -14,9 +14,9 @@ rym_auth <-
     if (!dir.exists(token.path)) {
       dir.create(token.path)
     }
-
+    
     # token save
-
+    
     if (new.user == FALSE && file.exists(paste0(paste0(token.path, "/", login, ".rymAuth.RData")))) {
       message("Load token from ", paste0(paste0(token.path, "/", login, ".rymAuth.RData")))
       load(paste0(token.path, "/", login, ".rymAuth.RData"))
@@ -39,7 +39,7 @@ rym_auth <-
         # add info about expire time and login
         token$expire_at <- Sys.time() + as.numeric(token$expires_in, units = "secs")
         token$username  <- login
-
+        
         # save auth token
         class(token) <- "RymToken"
         
@@ -53,7 +53,7 @@ rym_auth <-
         return(token)
       } else {
         message("Token expire in ", round(as.numeric(token$expire_at - Sys.time(), units = "days"), 0), " days")
-
+        
         # set login
         options(rym.user = login)
         
@@ -64,21 +64,25 @@ rym_auth <-
     browseURL(paste0("https://oauth.yandex.ru/authorize?response_type=code&client_id=5a87e45d5562421bb29bb9abd17321b3&redirect_uri=https://selesnow.github.io/rym/getToken/get_code.html&force_confirm=", as.integer(new.user), ifelse(is.null(login), "", paste0("&login_hint=", login))))
     # read auth code
     temp_code <- readline(prompt = "Enter authorize code:")
-
+    
     # check code
-    while (nchar(temp_code) != 7) {
-      message("The verification code you entered is not a 7-digit code, please try entering the code again.")
+    # check code - updated to 16 characters
+    while (nchar(gsub("[^0-9A-Za-z]", "", temp_code)) != 16) {
+      message("The verification code should be exactly 16 characters long (digits and letters), please try again.")
       temp_code <- readline(prompt = "Enter authorize code:")
     }
-
-    token_raw <- httr::POST("https://oauth.yandex.ru/token", body = list(
-      grant_type = "authorization_code",
-      code = temp_code,
-      client_id = "5a87e45d5562421bb29bb9abd17321b3",
-      client_secret = "04e7f096ce21483fb1c9861f68c017d7"
-    ), encode = "form")
-    # parsing
-    token <- content(token_raw)
+    
+    token_raw <- request("https://oauth.yandex.ru/token") %>%
+      req_body_form(
+        grant_type = "refresh_token",
+        refresh_token = token$refresh_token,
+        client_id = "5a87e45d5562421bb29bb9abd17321b3",
+        client_secret = "04e7f096ce21483fb1c9861f68c017d7"
+      ) %>%
+      req_perform()
+    
+    # Извлекаем JSON
+    token <- token_raw %>% resp_body_json()
     
     # token class
     class(token) <- "RymToken"
@@ -91,11 +95,11 @@ rym_auth <-
     if (!is.null(token$error_description)) {
       stop(paste0(token$error, ": ", token$error_description))
     }
-
+    
     # save token in file
     message("Do you want save API credential in local file (", paste0(token.path, "/", login, ".rymAuth.RData"), "), for use it between R sessions?")
     ans <- readline("y / n (recomedation - y): ")
-
+    
     if (tolower(ans) %in% c("y", "yes", "ok", "save")) {
       save(token, file = paste0(token.path, "/", login, ".rymAuth.RData"))
       message("Token saved in file ", paste0(token.path, "/", login, ".rymAuth.RData"))
